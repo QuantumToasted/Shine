ffa620869dbc7fe647c0f877258e5609c20eedb1
Daniel Jarski <30324210+QuantumToasted@users.noreply.github.com>
2021-05-05 21:41:11 -0500

    Added missing logging functionality.

---
+++ Common/DummyLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Shine.Common
+{
+    public sealed class DummyLogger<T> : ILogger<T>
+    {
+        private readonly ILogger _logger;
+
+        public DummyLogger(ILoggerFactory factory)
+        {
+            _logger = factory.CreateLogger(typeof(T).Name);
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            => _logger.Log(logLevel, eventId, state, exception, formatter);
+
+        public bool IsEnabled(LogLevel logLevel)
+            => _logger.IsEnabled(logLevel);
+
+        public IDisposable BeginScope<TState>(TState state)
+            => _logger.BeginScope(state);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using Disqord;
 using Disqord.Bot.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Shine.Common;
 
 namespace Shine
 {
@@ -16,6 +20,21 @@ namespace Shine
                 .ConfigureAppConfiguration(x =>
                 {
                     x.AddEnvironmentVariables("SHINE_");
+                })
+                .ConfigureLogging(x =>
+                {
+                    var logger = new LoggerConfiguration()
+                        .WriteTo.Console(
+                            outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}")
+                        .WriteTo.File("Logs/log_.txt",
+                            outputTemplate: "{Timestamp:HH:mm:ss} [{Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}",
+                            rollingInterval: RollingInterval.Day)
+                        .CreateLogger();
+                    
+                    x.AddSerilog(logger, true);
+        
+                    x.Services.Remove(x.Services.First(y => y.ServiceType == typeof(ILogger<>)));
+                    x.Services.AddSingleton(typeof(ILogger<>), typeof(DummyLogger<>));
                 })
                 .ConfigureServices(x =>
                 {
