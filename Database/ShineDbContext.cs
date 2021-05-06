8c2351773b2649277e2a0704f242eb22c73b14e4
Daniel Jarski <30324210+QuantumToasted@users.noreply.github.com>
2021-05-05 21:00:44 -0500

    Initial commit™

---
+++ Database/ShineDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shine.Database
+{
+    public sealed class ShineDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            // TODO: What DB provider?
+            // builder.UseSqlite("Data Source=./Data/Shine.db");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using Disqord;
+using Disqord.Bot.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Shine
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            using var host = new HostBuilder()
+                .ConfigureAppConfiguration(x =>
+                {
+                    x.AddEnvironmentVariables("SHINE_");
+                })
+                .ConfigureServices(x =>
+                {
+                    x.AddSingleton<Random>();
+                    x.AddSingleton<HttpClient>();
+                })
+                .ConfigureDiscordBot((context, bot) =>
+                {
+                    bot.Token = context.Configuration["TOKEN"];
+                    bot.OwnerIds = new Snowflake[]
+                    {
+                        167452465317281793,
+                        114926832440180738,
+                        176081685702639616 
+                    };
+                    bot.Prefixes = new[] {"s!"};
+                })
+                .Build();
+            
+            host.Run();
+        }
+    }
+}
